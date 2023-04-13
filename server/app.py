35ca8c145b3fbd148f7364fe5ca95fea416fe889
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-04-13 09:34:14 -0700

    all gets complete

--- server/app.py
+++ server/app.py
@@ -18,11 +18,24 @@ db.init_app(app)
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
-
+#GETS
 @app.route('/restaurants')
 def restaurants():
+    return [rest.to_dict(rules=('pizzas',)) for rest in Restaurant.query.all()]
 
-    pass
+@app.route('/pizzas')
+def pizza():
+    return [za.to_dict() for za in Pizza.query.all()]
+
+@app.route('/restaurants/<int:id>', methods=['GET'])
+def rest_by_id(id):
+    one_rest = Restaurant.query.filter_by(id=id).first()
+    if one_rest == None:
+        return make_response({"error": "Restaurant not found"}, 404)
+    elif request.method == 'GET':
+        return make_response(one_rest.to_dict(rules=('pizzas',)), 200)
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
