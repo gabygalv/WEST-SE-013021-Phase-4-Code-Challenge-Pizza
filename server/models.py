d1a84de8383f8cee8638246fc1667359d85daf70
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-04-13 09:21:45 -0700

    tables created and seeded

--- server/models.py
+++ server/models.py
@@ -10,4 +10,52 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant')
+
+    serialize_rules = ('-restaurants', '-restaurant_pizzas', '-created_at', '-updated_at')
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza')
+
+    serialize_rules = ('-pizzas', '-restaurant_pizzas', '-created_at', '-updated_at')
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key = True)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    serialize_rules = ('-created_at', '-updated_at')
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if 1 <= price <= 30:
+            return price
+        raise ValueError('Price must be between 1 and 30')
+
+
+
 # Add models here
